f62eab6639aefd73895b07f5113b6528babb034d
Damien <mail@damientimewell.com>
2019-11-27 17:12:59 +0100

    First part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const App = () => {
+  const [happiness, setHappiness] = useState('happy')
+  const [feeling, setFeeling] = useState('')
+  const [showResult, setShowResult] = useState(false)
+
+  // foo[0] = state item
+  // foo[1] = function to change the state item
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setShowResult(true)
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      {showResult && (
+        <div>
+          <h1>Here's your result!</h1>
+
+          <p>You are {happiness === 'happy' ? 'Super happy!' : 'Sad :('}</p>
+          <p>{feeling}</p>
+        </div>
+      )}
+
+      {!showResult && (
+        <form onSubmit={handleSubmit}>
+          <div className="feelings">
+            <label>
+              <input
+                type="radio"
+                value="happy"
+                onChange={() => setHappiness('happy')}
+                checked={happiness === 'happy'} />
+
+              <span role="img" aria-label="Happy face">
+                😄
+              </span>
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="sad"
+                onChange={() => setHappiness('sad')}
+                checked={happiness === 'sad'} />
+              <span role="img" aria-label="Sad face">
+                😞
+              </span>
+            </label>
+          </div>
+
+          <div className="thought">
+            <label>
+              How are you feeling?
+              <input type="text" value={feeling} onChange={(event) => setFeeling(event.target.value)} />
+            </label>
+          </div>
+
+          <button type="submit">
+            Send my feelings
+          </button>
+        </form>
+      )}
     </div>
   )
 }
